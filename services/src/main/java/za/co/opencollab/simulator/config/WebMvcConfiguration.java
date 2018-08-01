45c99084ac137f68da2f31209b2ca86a4ab363d3
Charl Thiem <charl.thiem@gmail.com>
2018-08-01 20:07:36 +0200

    Package for jar deployment

---
+++ services/src/main/java/za/co/opencollab/simulator/config/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package za.co.opencollab.simulator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("redirect:/index.html");
+	}
+}
